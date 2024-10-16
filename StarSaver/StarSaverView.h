2dbe6ca9c0e475ae230d6f203ed0cf704331ed1c
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2024-10-17 03:32:48 +1100

    Initial Commit

---
+++ StarSaver/StarSaverView.h
@@ -0,0 +1,12 @@
+//
+//  StarSaverView.h
+//  StarSaver
+//
+//  Created by Vino Rodrigues on 17/10/24.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface StarSaverView : ScreenSaverView
+
+@end
