40ca4a1c2dedfc9799d4748f71185ada9873b8a1
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2024-10-18 04:54:19 +1100

    .

--- StarSaver/StarSaverView.h
+++ StarSaver/StarSaverView.h
@@ -4,35 +4,38 @@
 #import <ScreenSaver/ScreenSaver.h>
 #import <Cocoa/Cocoa.h>
 
+// size of stars as in resouce files  // TODO : Refactor names
+#define BMX 14
+#define BMY 14
+
+typedef NS_ENUM(NSInteger, StarState) {
+  StarStateGone,
+  StarStateNormal,
+  StarStateExploding,
+  StarStateNovaState1,
+  StarStateNovaState2,
+  StarStateNovaState3
+};
+
 @interface Star : NSObject
-
-@property (nonatomic, assign) NSPoint position;
-@property (nonatomic, assign) NSInteger novaStage;  // 0 means not going Nova, 1-4 means in Nova stages
-
+  @property (nonatomic, assign) NSPoint position;
+  @property (nonatomic, assign) StarState state;
 @end
 
 @interface StarSaverView : ScreenSaverView
+  @property (nonatomic, strong) NSArray *starImages;        // Array to hold star images (star1.png to star5.png)
+  @property (nonatomic, strong) NSMutableArray *stars;      // Array to hold stars (Star objects)
+  @property (nonatomic, assign) NSInteger numberOfStars;    // Number of stars
+  @property (nonatomic, assign) NSInteger starHead;         // Current star being processed
+  @property (nonatomic, assign) NSInteger novaProbability;  // Probability for Nova (1 in X chance)
+  @property (nonatomic, assign) NSInteger animationTiming;  // Animation timing in milliseconds
 
-@property (nonatomic, strong) NSArray *starImages;        // Array to hold star images (star1.png to star5.png)
-@property (nonatomic, strong) NSMutableArray *stars;      // Array to hold stars (Star objects)
-@property (nonatomic, strong) NSMutableArray *novaStars;  // Array to hold stars currently in Nova
-@property (nonatomic, strong) NSTimer *moveTimer;         // Timer to control star movement and animation
-@property (nonatomic, assign) NSInteger numberOfStars;    // Number of stars
-@property (nonatomic, assign) NSInteger novaProbability;  // Probability for Nova (1 in X chance)
-@property (nonatomic, assign) NSInteger animationTiming;  // Animation timing in milliseconds
-@property (nonatomic, assign) NSSize starSize;            // Size of each star
+  @property (nonatomic, strong) NSTimer *timer;             // Timer to control star movement and animation
 
-@property (nonatomic, assign) NSInteger ticksBeforeMove;  // Clock movement frequency
-@property (nonatomic, strong) NSString *dateFormat;       // Date format string for the clock
-@property (nonatomic, strong) NSColor *clockColor;        // Clock font color
-@property (nonatomic, strong) NSFont *clockFont;          // Clock font
-
-- (void)doInit;
-- (void)generateStarPositions;
-- (void)updateStars;
-- (void)moveOrNovaRandomStar;
-- (void)loadConfig;  // Method to load configuration from JSON
-
+  - (NSPoint)randomPosition;
+  - (void)internalInit;
+  - (void)drawStarAt:(NSInteger)index;
+  - (void)timerTick;
 @end
 
 #endif /* StarSaverView_h */
--- StarSaver/StarSaverView.m
+++ StarSaver/StarSaverView.m
@@ -1,232 +1,318 @@
+/*
+ * Commander Starry Night Screen Saver
+ * A clone of the old Norton Commander 4 Saver
+ *
+ * ©️ 2024 Vino Rodrigues
+ * 
+ * @see: https://developer.apple.com/documentation/screensaver?language=objc
+ */
+
 #import "StarSaverView.h"
+#import <Foundation/Foundation.h>
+#import <math.h>
 
 @implementation Star
-
 @end
 
 @implementation StarSaverView
 
+// ------------------------------
 // Initializer for normal and preview mode
+// ------------------------------
 - (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview {
-    self = [super initWithFrame:frame isPreview:isPreview];
-    if (self) {
-        [self doInit];
-    }
-    return self;
+  self = [super initWithFrame:frame isPreview:isPreview];
+  if (self) {
+    [self internalInit];
+  }
+  return self;
 }
 
+// ------------------------------
 // Initializer for coder-based initialization
+// ------------------------------
 - (instancetype)initWithCoder:(NSCoder *)coder {
-    self = [super initWithCoder:coder];
-    if (self) {
-        [self doInit];
-    }
-    return self;
+  self = [super initWithCoder:coder];
+  if (self) {
+    [self internalInit];
+  }
+  return self;
 }
 
-// Consolidated initialization method
-- (void)doInit {
-    // Load configuration from the JSON file
-    [self loadConfig];
-    
-    // Set default values if config is invalid
-    if (self.numberOfStars <= 0) {
-        self.numberOfStars = 100;  // Default to 100 stars
+// ------------------------------
+// Load the screen saver settings
+// ------------------------------
+// TODO : Change this to use the `ScreenSaverDefaults` class
+- (void)loadPreferences {
+  // Load configuration from the JSON file
+  NSString *configFilePath = [[NSBundle bundleForClass:[self class]] pathForResource:@"config" ofType:@"json"];
+  
+  if (configFilePath) {
+    NSData *data = [NSData dataWithContentsOfFile:configFilePath];
+    NSError *error;
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+      
+    if (!error && json) {
+      NSNumber *starCount = json[@"starCount"];
+      NSNumber *novaProbability = json[@"novaProbability"];
+      NSNumber *animationTiming = json[@"animationTiming"];
+          
+      if (starCount) {
+        self.numberOfStars = [starCount integerValue];
+      }
+      if (novaProbability) {
+        self.novaProbability = [novaProbability integerValue];
+      }
+      if (animationTiming) {
+        self.animationTiming = [animationTiming integerValue];
+      }
     }
-    if (self.novaProbability <= 0) {
-        self.novaProbability = 40;  // Default Nova probability to 1 in 40
-    }
-    if (self.animationTiming <= 0) {
-        self.animationTiming = 250;  // Default animation timing to 250ms
-    }
-    
-    // Adjust star count and animation timing based on screen width
-    CGFloat screenWidth = self.bounds.size.width;
-    if (screenWidth <= 600) {
-        self.numberOfStars = 5;          // Set to 5 stars for smaller screens
-        self.animationTiming = 2000;     // Set to 2 seconds for smaller screens
-    }
-    
-    // Set the size of each star (14x14 pixels)
-    self.starSize = NSMakeSize(14, 14);
-    
-    // Load all the star images (star1.png to star5.png)
-    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
-    NSMutableArray *loadedImages = [NSMutableArray array];
-    for (int i = 1; i <= 5; i++) {
-        NSString *imagePath = [bundle pathForResource:[NSString stringWithFormat:@"star%d", i] ofType:@"png"];
-        if (imagePath) {
-            NSImage *image = [[NSImage alloc] initWithContentsOfFile:imagePath];
-            if (image) {
-                [loadedImages addObject:image];
-            }
-        }
-    }
-    self.starImages = [loadedImages copy];
-    
-    // Initialize the stars array
-    self.stars = [NSMutableArray array];
-    self.novaStars = [NSMutableArray array];  // Track stars that are in Nova
-    
-    // Generate star positions based on the screen size
-    [self generateStarPositions];
-    
-    // Set up a timer to move or animate stars based on the animation timing
-    self.moveTimer = [NSTimer scheduledTimerWithTimeInterval:(self.animationTiming / 1000.0)
-                                                      target:self
-                                                    selector:@selector(updateStars)
-                                                    userInfo:nil
-                                                     repeats:YES];
+  }
+
+  // Set default values if config is invalid
+  if (self.numberOfStars <= 0) {
+    self.numberOfStars = 100;  // Default to 100 stars
+  }
+  if (self.novaProbability <= 0) {
+    self.novaProbability = 40; // Default Nova probability to 1 in 40
+  }
+  if (self.animationTiming <= 0) {
+    self.animationTiming = 250; // Default animation timing to 250ms
+  }
+  
+  // TODO : Adjust for sane min / max values
 }
 
-// Method to load configuration from config.json
-- (void)loadConfig {
-    NSString *configFilePath = [[NSBundle bundleForClass:[self class]] pathForResource:@"config" ofType:@"json"];
-    
-    if (configFilePath) {
-        NSData *data = [NSData dataWithContentsOfFile:configFilePath];
-        NSError *error;
-        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
-        
-        if (!error && json) {
-            NSNumber *starCount = json[@"starCount"];
-            NSNumber *novaProbability = json[@"novaProbability"];
-            NSNumber *animationTiming = json[@"animationTiming"];
-            
-            if (starCount) {
-                self.numberOfStars = [starCount integerValue];
-            }
-            if (novaProbability) {
-                self.novaProbability = [novaProbability integerValue];
-            }
-            if (animationTiming) {
-                self.animationTiming = [animationTiming integerValue];
-            }
-        }
-    }
+// ------------------------------
+// Generate random co-ordinates
+// ------------------------------
+- (NSPoint)randomPosition {
+  NSInteger cols = floor( self.bounds.size.width / BMX );
+  NSInteger rows = floor( self.bounds.size.height / BMY );
+
+  CGFloat x = SSRandomIntBetween(0, (int)cols);
+  CGFloat y = SSRandomIntBetween(0, (int)rows);
+  NSPoint point = NSMakePoint(x, y);
+
+  return point;
 }
 
-// Generate random positions for the stars based on the screen size
-- (void)generateStarPositions {
-    // Get the size of the screen
-    CGFloat screenWidth = self.bounds.size.width;
-    CGFloat screenHeight = self.bounds.size.height;
+// ------------------------------
+// Consolidated initialisation methods
+// ------------------------------
+- (void)internalInit {
+  // Get the size of the screen
+  CGFloat screenWidth = self.bounds.size.width;
+  
+  // ----- Initialize RandomSeed -----
+  // Use the current time to set a unique seed
+  srand((unsigned int)time(NULL));  // used by `rand()`
+  srandom((unsigned int)time(NULL));  // used by `random()`
+
+  // ----- Preferences -----
+  
+  // Load user configuration
+  [self loadPreferences];
+  
+  // Adjust star count and animation timing based on screen width
+  if (self.isPreview && screenWidth <= 639) {
+    self.numberOfStars = 5;      // Set to 5 stars for smaller screens
+    self.animationTiming = 2000; // Set to 2 seconds for smaller screens
+  }
     
-    // Calculate how many 14x14 cells fit in the screen
-    NSInteger cols = screenWidth / self.starSize.width;
-    NSInteger rows = screenHeight / self.starSize.height;
-    
-    // Create a grid of possible positions
-    NSMutableArray *possiblePositions = [NSMutableArray array];
-    
-    for (NSInteger row = 0; row < rows; row++) {
-        for (NSInteger col = 0; col < cols; col++) {
-            CGFloat x = col * self.starSize.width;
-            CGFloat y = row * self.starSize.height;
-            NSPoint point = NSMakePoint(x, y);
-            [possiblePositions addObject:[NSValue valueWithPoint:point]];
-        }
+  // Load all the star images (`star1.png` to `star5.png`)
+  NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+  NSMutableArray *loadedImages = [NSMutableArray array];
+  for (int i = 1; i <= 5; i++) {
+    NSString *imagePath = [bundle pathForResource:[NSString stringWithFormat:@"star%d", i] ofType:@"png"];
+    if (imagePath) {
+      NSImage *image = [[NSImage alloc] initWithContentsOfFile:imagePath];
+      if (image) {
+        [loadedImages addObject:image];
+      }
     }
-    
-    // Shuffle the positions for randomness
-    for (NSUInteger i = possiblePositions.count - 1; i > 0; i--) {
-        NSUInteger j = arc4random_uniform((uint32_t)(i + 1));
-        [possiblePositions exchangeObjectAtIndex:i withObjectAtIndex:j];
-    }
-    
-    // Create stars and assign the first N positions
-    for (NSInteger i = 0; i < self.numberOfStars; i++) {
-        Star *star = [[Star alloc] init];
-        star.position = [[possiblePositions objectAtIndex:i] pointValue];
-        star.novaStage = 0;  // 0 means not going Nova
-        [self.stars addObject:star];
-    }
+  }
+  self.starImages = [loadedImages copy];
+  
+  // ----- Initialise the Stars -----
+  
+  self.starHead = 0;  // Start the star "buffer" at the begining
+
+  // Initialize the stars array
+  self.stars = [NSMutableArray array];
+
+  // Build the star array with initial random positions
+  for (NSInteger i = 0; i < self.numberOfStars; i++) {
+    Star *star = [[Star alloc] init];
+//    if ((true)) {  // set to `true` if you want the stars to gradually apear from an empty screen
+      // star.position = NSMakePoint(0, 0);
+      star.state = StarStateGone;
+//    } else {
+//      star.position = self.randomPosition;
+//      star.state = StarStateNormal;
+//    }
+    [self.stars addObject:star];
+  }
+  
+  NSTimeInterval interval = self.animationTiming / 1000.0;
+  [self setAnimationTimeInterval:interval];
 }
 
-// Update the state of stars (move or progress Nova stages)
-- (void)updateStars {
-    // First, progress all stars currently in Nova stages
-    NSMutableArray *novaStarsToRemove = [NSMutableArray array];  // Temporary array to store stars that finish Nova
-    
-    for (Star *novaStar in self.novaStars) {
-        // Progress through Nova stages
-        if (novaStar.novaStage < 4) {
-            novaStar.novaStage++;
-        } else {
-            // Once the star finishes Nova, move it to a new random position and remove it from the Nova list
-            [self moveStarToNewPosition:novaStar];
-            [novaStarsToRemove addObject:novaStar];
-        }
-    }
-    
-    // Remove stars that have completed their Nova
-    [self.novaStars removeObjectsInArray:novaStarsToRemove];
-    
-    // Now pick a random non-Nova star and decide whether to move it or start Nova
-    [self moveOrNovaRandomStar];
-    
-    // Redraw the view with the updated star positions
-    [self setNeedsDisplay:YES];
+// ------------------------------
+- (void)startAnimation {
+  [super startAnimation];
+  
+  // Create a timer that fires {every X} and calls the 'timerTick' method
+
+  NSTimeInterval interval = self.animationTiming / 1000.0;
+  self.timer = [NSTimer scheduledTimerWithTimeInterval:interval
+                                                target:self
+                                              selector:@selector(timerTick)
+                                              userInfo:nil
+                                               repeats:YES];
+  
+  [self setNeedsDisplay:YES];  // redraw the whole screen
 }
 
-// Move or trigger Nova for a random star
-- (void)moveOrNovaRandomStar {
-    // Find a random star that is not in Nova
-    NSUInteger randomIndex = arc4random_uniform((uint32_t)self.stars.count);
-    Star *star = [self.stars objectAtIndex:randomIndex];
-    
-    // Check if the star will go Nova (1 in novaProbability chance)
-    if (arc4random_uniform((uint32_t)self.novaProbability) == 0) {
-        star.novaStage = 1;  // Start the Nova animation
-        [self.novaStars addObject:star];  // Track this star in the Nova list
-    } else {
-        // If not going Nova, move the star
-        [self moveStarToNewPosition:star];
-    }
+// ------------------------------
+- (void)stopAnimation {
+  [super stopAnimation];
+
+  // Invalidate the timer when stopping the screen saver
+  [self.timer invalidate];
+  self.timer = nil;
 }
 
-// Move the star to a new random position
-- (void)moveStarToNewPosition:(Star *)star {
-    // Get the size of the screen
-    CGFloat screenWidth = self.bounds.size.width;
-    CGFloat screenHeight = self.bounds.size.height;
-    
-    // Calculate how many 14x14 cells fit in the screen
-    NSInteger cols = screenWidth / self.starSize.width;
-    NSInteger rows = screenHeight / self.starSize.height;
-    
-    // Pick a new random position
-    CGFloat newX = arc4random_uniform((uint32_t)cols) * self.starSize.width;
-    CGFloat newY = arc4random_uniform((uint32_t)rows) * self.starSize.height;
-    star.position = NSMakePoint(newX, newY);
-    
-    // Reset the Nova stage
-    star.novaStage = 0;
+// ------------------------------
+// Draw star at n-th position
+// ------------------------------
+- (void)drawStarAt:(NSInteger)index {
+  NSImage *starImage;
+  Star *star = [self.stars objectAtIndex:(index)];  // Get the start at the head position
+
+  // Select the appropriate star image based on Nova stage
+  switch (star.state) {
+    case StarStateNormal:
+      starImage = self.starImages[0];
+      break;
+    case StarStateExploding:
+      starImage = self.starImages[1];
+      break;
+    case StarStateNovaState1:
+      starImage = self.starImages[2];
+      break;
+    case StarStateNovaState2:
+      starImage = self.starImages[3];
+      break;
+    case StarStateNovaState3:
+      starImage = self.starImages[4];
+      break;
+    default:
+      starImage = nil;
+      break;
+  }
+  
+  // Draw the star
+  if (starImage != nil) {
+    NSRect starRect = NSMakeRect(star.position.x * BMX, star.position.y * BMY, BMX, BMY);
+    [starImage drawInRect:starRect];
+  }
 }
 
-// Draw the stars at the assigned positions
+/*
+// ------------------------------
+// Erase the star potition
+// ------------------------------
+- (void)eraseCellAt:(NSPoint)pos {
+  // TODO: Check if another star shares the position
+  [[NSColor redColor] setFill];  // TODO: FIXME: blackColor
+  NSRect rect = NSMakeRect(pos.x * BMX, pos.y * BMY, BMX, BMY);
+  NSRectFill(rect);
+}
+*/
+
+// ------------------------------
+// Draw the initial state
+// ------------------------------
 - (void)drawRect:(NSRect)rect {
-    [super drawRect:rect];
-    
-    // Fill the background with black
-    [[NSColor blackColor] setFill];
-    NSRectFill(rect);  // Fill the entire screen with black
-    
-    // Draw the stars at the random positions
-    for (Star *star in self.stars) {
-        NSImage *starImage;
-        
-        // Select the appropriate star image based on Nova stage
-        if (star.novaStage > 0) {
-            starImage = self.starImages[star.novaStage];  // star2.png to star5.png
-        } else {
-            starImage = self.starImages[0];  // star1.png (default)
-        }
-        
-        // Draw the star
-        NSRect starRect = NSMakeRect(star.position.x, star.position.y, self.starSize.width, self.starSize.height);
-        [starImage drawInRect:starRect];
-    }
+  [super drawRect:rect];
+
+  // Fill the background with black
+  [[NSColor blackColor] setFill];
+  NSRectFill(rect);  // Fill the entire screen with black
+  
+  // Draw all the stars at the random positions
+  for (NSInteger i = 0; i < self.numberOfStars; i++) {
+    [self drawStarAt:i];
+  }
+}
+
+// ---------------------------------
+// Abstracting Timer Ticks here
+// ---------------------------------
+- (void)timerTick {
+  
+  NSInteger index = self.starHead;
+  
+  Star *star = [self.stars objectAtIndex:(index)];  // Get the start at the head position
+  StarState newState = StarStateNormal;
+
+  switch (star.state) {
+    case StarStateNormal:
+      newState = StarStateExploding;
+      break;
+    case StarStateExploding:
+      if (SSRandomIntBetween(0, (int)self.novaProbability) == 0) {
+        newState = StarStateNovaState1;
+      } else {
+        newState = StarStateGone;
+      }
+      break;
+    case StarStateNovaState1:
+      newState = StarStateNovaState2;
+      break;
+    case StarStateNovaState2:
+      newState = StarStateNovaState3;
+      break;
+    case StarStateNovaState3:
+      newState = StarStateGone;
+      break;
+    default: // StarSateGone
+      star.position = self.randomPosition;  // new position
+  
+      // inc the header, i.e. move on to the next star
+      self.starHead++;
+      if (self.starHead >= self.numberOfStars) {
+        self.starHead = 0;  // restart
+      }
+      break;
+  }
+
+  star.state = newState;
+
+  // Force the view to redraw
+  [self setNeedsDisplay:YES];
+}
+
+//// ------------------------------
+//// Gets called repeatedly to draw states on timer ticks
+//// ------------------------------
+//- (void)animateOneFrame {
+//  [self timerTick];
+//  return;
+//}
+
+// ------------------------------
+// TODO: Later
+// ------------------------------
+- (BOOL)hasConfigureSheet {
+  return NO;
+}
+
+// ------------------------------
+// TODO: Later
+// ------------------------------
+- (NSWindow*)configureSheet {
+  return nil;
 }
 
 @end
